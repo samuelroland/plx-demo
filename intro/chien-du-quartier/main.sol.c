5ef728a5bd3c14a860ca73a1b57cbd89e40d7651
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-08-31 21:21:39 +0200

    Delete exo chien du quartier in intro

--- intro/chien-du-quartier/main.sol.c
+++ intro/chien-du-quartier/main.sol.c
@@ -3,9 +3,7 @@
 int main(int argc, char *argv[]) {
     if (argc == 2)
         printf("Le chien %s est sympa\n", argv[1]);
-    else if (argc == 3)
-        printf("Le chien %s est sympa et dit '%s'\n", argv[1], argv[2]);
-    else if (argc > 3)
+    else if (argc > 2)
         printf("Trop d'info\n");
     else
         printf("Pas de chien...\n");
