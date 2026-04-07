4b16d8f78a6eb5c7fd152ffd76ec97802d688946
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-04-07 18:43:05 +0200

    Add simple math exo to demonstrate math display

---
+++ math/factorial/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// TODO
+
+int main(int argc, char *argv[]) {
+    cout << "25 Celsius is " << celsius_to_fahrenheit(25) << " Fahrenheit" << endl;
+    cout << "100 Fahrenheit donne " << fahrenheit_to_celsius(100) << " Celsius" << endl;
+}
---
+++ math/factorial/main.sol.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+double celsius_to_fahrenheit(double celsius) {
+    return 9. / 5. * celsius + 32;
+}
+double fahrenheit_to_celsius(double fahrenheit) {
+    return 5. / 9. * (fahrenheit - 32);
+}
+
+int main(int argc, char *argv[]) {
+    cout << "25 Celsius is " << celsius_to_fahrenheit(25) << " Fahrenheit" << endl;
+    cout << "100 Fahrenheit donne " << fahrenheit_to_celsius(100) << " Celsius" << endl;
+}
